bab7c2058963c99f480710e539274053be68be5b
sammy2201 <bhavanish2000@gmail.com>
2024-02-26 19:52:35 +0100

    new comment added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
-import MainContent from './components/MainContent';
-import logo from './assets/images/logo.png';
+import MainContent from "./components/MainContent";
+import logo from "./assets/images/logo.png";
+
+//new comments
 
 function App() {
   return (
